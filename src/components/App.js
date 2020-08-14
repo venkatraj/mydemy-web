1759f0af438ee8f7530c820e579bc1d5e4cb35ce
unknown <venkat@codinggeek.com>
2020-08-14 10:58:03 +0530

    basic routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
-import logo from '../logo.svg';
-import '../styles/App.css';
+
 import Header from './Header';
 import Menu from './Menu';
 import Footer from './Footer';
@@ -11,22 +10,6 @@ function App() {
     <Grid>
       <Header />
       <Menu />
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
       <Footer />
     </Grid>
   );
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,39 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import Login from './Login';
+import Signup from './Signup';
 
-const Menu = () => <Typography component="h1">Menu</Typography>;
+const Menu = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default Menu;
